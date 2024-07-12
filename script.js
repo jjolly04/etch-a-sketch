4d71e781628caaf8714002d82117468d263990be
jjolly04 <judejolly2004@gmail.com>
2024-07-11 19:46:17 -0500

    Add feature to leave color trail on hover in grid
    Added event listeners to change the background color of each square on
    a hover. This allows you to 'draw' a pixelated trail in the grid with
    your mouse.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const container = document.querySelector(".container");
 
-
 for (let i = 0; i < 16; i++) {
   const gridColumn = document.createElement("div");
   gridColumn.className = "gridColumn";
@@ -10,14 +9,19 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const gridSquare = document.createElement("div");
     gridSquare.style.border = "1px solid grey";
-    gridSquare.className = 'gridSquare';
+    gridSquare.className = "gridSquare";
 
-    gridSquare.style.padding = '20px';
+    gridSquare.style.padding = "20px";
     gridColumn.appendChild(gridSquare);
-
+  }
 }
 
-}
+//make function to change background color of grid squares on hover
 
+const allGridSquares = document.querySelectorAll(".gridSquare");
 
-
+allGridSquares.forEach((square) => {
+  square.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
+});
