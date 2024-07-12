a6fd009a7bea2c03609a7519bc25cc82aac98511
jjolly04 <judejolly2004@gmail.com>
2024-07-11 19:34:56 -0500

    Make 16x16 grid using js, aligned with css

---
+++ script.js
@@ -0,0 +1,23 @@
+//adding a 16x16 grid square to the site
+
+const container = document.querySelector(".container");
+
+
+for (let i = 0; i < 16; i++) {
+  const gridColumn = document.createElement("div");
+  gridColumn.className = "gridColumn";
+  container.appendChild(gridColumn);
+  for (let j = 0; j < 16; j++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.style.border = "1px solid grey";
+    gridSquare.className = 'gridSquare';
+
+    gridSquare.style.padding = '20px';
+    gridColumn.appendChild(gridSquare);
+
+}
+
+}
+
+
+
