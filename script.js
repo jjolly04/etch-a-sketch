d16f109240d047f4516d5b778d5802121c5ab317
jjolly04 <judejolly2004@gmail.com>
2024-07-12 18:29:53 -0500

    Add ability to resize grid and maintain total size
    Added the ability to determine the grid's dimensions while also making
    sure the total size of the grid is the same regardless of dimensions.

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.className = "gridSquare";
-  container.appendChild(gridSquare);
-  gridSquare.style.border = '1px solid grey';
-
-
-
+for (let i = 0; i < 16; i++) {
+  const gridColumn = document.createElement("div");
+  gridColumn.className = "gridColumn";
+  container.appendChild(gridColumn);
+  for (let j = 0; j < 16; j++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.className = "gridSquare";
+    gridColumn.appendChild(gridSquare);
   }
-
+}
 
 //make function to change background color of grid squares on hover
 
